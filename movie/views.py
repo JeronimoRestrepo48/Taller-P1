5b67652c863d1e995d64d120c0e93aafc86175fb
Jerónimo Restrepo <jeronimorestrepo48@gmail.com>
2026-02-05 10:50:13 -0500

    cambios

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
 	# return HttpResponse('<h1>Welcome to Home Page</h1>')
-	return render(request, 'home.html', {'name': 'John'})
+	return render(request, 'home.html', {'name': 'Jerónimo Restrepo'})
 
 def about(request):
 	return HttpResponse('<h1>Welcome to About Page</h1>')
